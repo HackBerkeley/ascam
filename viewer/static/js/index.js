c438d08204f76bbe4279f5baab260972915e101a
Sharad Vikram <sharad.vikram@gmail.com>
2012-08-25 22:21:02 -0700

    added in framework code

--- viewer/static/js/index.js
+++ viewer/static/js/index.js
@@ -1,11 +1,5 @@
 (function() {
-  var client = new BinaryClient('ws://localhost:9001');
-  var stream;
-
-  client.on('open', function() {
-    stream = client.createStream({ type : "viewer" });
-    stream.on('data', function(data) {
-      $("body").prepend("<img src='"+data+"'></img>");
-    });
+  events.on('image', function(image) {
+    console.log(image);
   });
 })();
---
+++ viewer/static/js/photostream.js
@@ -0,0 +1,27 @@
+(function() {
+  var triggers = {};
+  window.events = {
+    on : function(type, callback) {
+      if (!triggers.type) {
+        triggers[type] = [];
+      }
+      triggers[type].push(callback);
+    }
+  };
+  var trigger = function(type, image) {
+    for (var i in triggers[type]){
+      if (typeof(triggers[type][i]) == "function") {
+        triggers[type][i](image);
+      }
+    }
+  } 
+  var client = new BinaryClient('ws://localhost:9001');
+  var stream;
+
+  client.on('open', function() {
+    stream = client.createStream({ type : "viewer" });
+    stream.on('data', function(data) {
+      trigger("image", data);
+    });
+  });
+})();
