954f6ff34fa29b6a2b4ce8d24e8415165a827818
Siddhartho Bhattacharya <sid_gogol@hotmail.com>
2012-08-28 00:20:46 -0700

    first working version with filters

--- viewer/static/js/index.js
+++ viewer/static/js/index.js
@@ -1,40 +1,78 @@
 
 (function() {
+
   $(document).ready(function(){
-    images = [];
+    elements = [];
+    filters = [];
     events.on('image', function(image) {
       addImg(image);
     });
-    function render() {
-      for(var i = 0; i<dim; i++) {
-        for(var j = 0; j<dim; j++) {
-          var index = i*dim + j;
-          if(index<images.length) {
-            $("#"+i+""+j).attr("src",images[index]);
-          }
-        }
-      }
-    }
     
-    function addImg(image) {
-      console.log(image);
-      images.unshift(image);
-      render();
-    }
     var dim = 4;
     var size = 500;
     var padoffset = 40;
-    $("#wrapper").width(size*4/3 + padoffset);
+    var aspectratio = 4/3;
+    var wrapper = $("#wrapper");
+    var height = size/dim;
+
+    
+    function render() {
+      for(var i=0; i<elements.length; i++) {
+        elements[i].css("display","block");
+        console.log(elements[i].val());
+        $("#div"+i).append(elements[i]);
+      }
+    }
+    
+    
+    
+    function imgFilter(image, el) {
+      console.log("Filtering image:",image);
+      Caman(image, "#canvas"+image.substring(4,image.length-4), function () {
+        this
+          .saturation(20)
+          .gamma(1.4)
+          .vintage()
+          .contrast(5)
+          .exposure(15)
+          .vignette(300, 60)
+          .render(function() {
+            render();
+          })
+      });  
+    }
+    
+    //DEBUG
+    window.addImg = addImg;
+    
+    function addImg(image) {
+      console.log("Adding image:",image);
+      
+      var canv = $("<canvas id='canvas"+image.substring(4,image.length-4)+"'/>", { width : height*aspectratio + "px", height : height+"px"});
+      canv.css("width", height*aspectratio + "px");
+      canv.css("height", height+"px");
+      canv.css("display","none");
+      elements.unshift(canv);
+      console.log(elements);
+      if(elements.length > dim*dim) {
+        elements.pop();
+      }
+      $("body").append(canv)
+
+      imgFilter(image);
+    }
+    wrapper.width(size*aspectratio + padoffset);
     var height = size/dim
     for(var i = 0; i<dim; i++) {
       for(var j = 0; j<dim; j++) {
-        var img = $("<img id='" + i + "" + j + "'/>");
-        img.addClass("tile");
-        img.css("width", height*(4/3) + "px");
-        img.css("height", height + "px");
-        $("#wrapper").append(img);
+      var index = i*dim + j;
+        var div = $("<div id='div" + index + "'/>");
+        div.addClass("tile");
+        div.css("width", height*aspectratio + "px");
+        div.css("height", height + "px");
+        wrapper.append(div);
       }
-      $("#wrapper").append("<br/>");
+      wrapper.append("<br/>");
     }
   });
   
