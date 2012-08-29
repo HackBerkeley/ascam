fdbab711ca602bd73dc751689fbfdea1365475f4
Michelle Bu <analogmidnight@gmail.com>
2012-08-29 14:24:56 -0700

    frontend finished

--- viewer/static/js/photostream.js
+++ viewer/static/js/photostream.js
@@ -8,12 +8,22 @@
     stream.on('data', function(data) {
       numImages++;
       var image = $('<img id="' + numImages + '" height="' + $(window).height() + '" src="' + data + '">');
+      image.css({ 'position': 'absolute', 'left': 0, 'display': 'none' })
       var wrapper = '#wrapper';//numImages % 2 == 0 ? '#evenwrapper' : '#oddwrapper';
-      console.log(wrapper);
       if (numImages > 2) {
-        $('#' + (numImages - 2)).remove();
+        $('#' + (numImages - 2)).animate({ 'left': $(window).height() * (8/3) }, function() {
+          $(this).remove();
+        });
+      }
+      if (numImages > 1) {
+        $('#' + (numImages - 1)).animate({ 'left': $(window).height() * (4/3) }, function() {
+          $(wrapper).append(image);
+          image.fadeIn('fast');
+        });
+        return;
       }
       $(wrapper).append(image);
+      image.fadeIn('fast');
     });
   });
 })();
