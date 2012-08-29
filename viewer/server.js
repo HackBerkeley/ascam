efc80c2699df6e8b6b435c63d8e12e381f148c03
ericz <really.ez@gmail.com>
2012-08-29 13:17:11 -0700

    fixes

--- viewer/server.js
+++ viewer/server.js
@@ -3,30 +3,29 @@ var app = express.createServer();
 var fs = require('fs');
 
 var BinaryServer = require('binaryjs').BinaryServer;
-var rooms = {};
+
 var id=0;
 var cl;
 
 // Start Binary.js server
-var server = BinaryServer({port: 9001});
+var server = new BinaryServer({port: 9001});
 // Wait for new user connections
 server.on('connection', function(client){
   client.on('error', function(e) {
-    console.log(arguments);
+    console.log('Error occured', e.message, e.stack);
   });
   client.on('stream', function(stream, meta){ 
     if (meta && meta.type == "viewer") {
       cl = stream;
       console.log("Detected client: ", stream);
     } else {
-      console.log("Receiving image data");
+      console.log("Receiving image " + id);
       var name = "img/"+id+"-"+(new Date()).getTime()+".jpg";
       var ws = fs.createWriteStream('static/'+name);
-      stream.pipe(ws);
       ws.on('close', function() {
-        console.log("done writing image");
         notifyClient(name);
       });
+      stream.pipe(ws);
       id++;
     }
   });
@@ -37,7 +36,6 @@ app.use(express.static(__dirname+"/static/"));
 app.listen(8000);
 
 var notifyClient = function(name) {
-  console.log("notifying client");
   if (cl) {
     cl.write(name);
   }
